fbdedaf0d1b85af12fc12a7fd3cdf371ea35f8d7
Ali Hamrani <ali.hamrani200@gmail.com>
2023-02-01 18:23:25 +0330

    updatePassword method added

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from messages import *
 @method
 def createUser(username, password) -> Result:
     try:
-        out = os.system(f"net user {username} {password} /add")
+        out = os.system(f'net user "{username}" "{password}" /add')
         if out != 0:
             return Error(code=123, message=ErrUserCreationFailed)
 
         out = os.system(
-            f'net localgroup "Remote Desktop Users" {username} /add')
+            f'net localgroup "Remote Desktop Users" "{username}" /add')
         if out != 0:
             return Error(code=123, message=ErrUserCreationFailed)
 
@@ -23,7 +23,7 @@ def createUser(username, password) -> Result:
 @method
 def deleteUser(username) -> Result:
     try:
-        out = os.system(f"net user {username} /delete")
+        out = os.system(f'net user "{username}" /delete')
         if out != 0:
             return Error(code=123, message=ErrUserDelete)
         return Success(MsgUserDeleted)
@@ -34,7 +34,7 @@ def deleteUser(username) -> Result:
 @method
 def activeUser(username) -> Result:
     try:
-        out = os.system(f"net user {username} /active:YES")
+        out = os.system(f'net user "{username}" /active:YES')
         if out != 0:
             return Error(code=123, message=ErrActiveUser)
         return Success(MsgActiveUser)
@@ -45,12 +45,22 @@ def activeUser(username) -> Result:
 @method
 def disableUser(username) -> Result:
     try:
-        out = os.system(f"net user {username} /active:NO")
+        out = os.system(f'net user "{username}" /active:NO')
         if out != 0:
             return Error(code=123, message=ErrDisableUser)
         return Success(MsgDisableUser)
     except:
         return Error(code=123, message=ErrDisableUser)
+
+@method
+def updatePassword(username, password) -> Result:
+    try:
+        out = os.system(f'net user "{username}" "{password}"')
+        if out != 0:
+            return Error(code=123, message=ErrUpdatePassword)
+        return Success(MsgUpatePassword)
+    except:
+        return Error(code=123, message=ErrUpdatePassword)
 
 
 if __name__ == "__main__":
